bf5d050daed23b4092fcffd4b9e999d388c5fbe4
Manny Cortez Studios <emailme@mannycortez.com>
2020-10-27 03:54:29 -0700

    Initial commit

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,51 @@
+import path from 'path'
+import express from 'express'
+import multer from 'multer'
+import cloud from 'cloudinary'
+
+const cloudinary = cloud
+
+const router = express.Router()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename(req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+function checkFileType(file, cb) {
+    const filetypes = /jpg|jpeg|png/
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = filetypes.test(file.mimetype)
+
+    if(extname && mimetype) {
+        return cb(null, true)
+    } else {
+        cb('Images only!')
+    }
+}
+
+const upload = multer({
+    storage,
+    fileFilter: function(req, file, cb) {
+        checkFileType(file, cb)
+    }
+})
+
+router.post('/', upload.single('image'), async(req, res) => {
+    const uploadPhoto = await cloud.uploader.upload(`${req.file.path}`)
+    console.log(uploadPhoto)
+    console.log(uploadPhoto.url)
+    res.send(uploadPhoto.url)
+})
+
+export default router
